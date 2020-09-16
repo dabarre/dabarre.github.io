c6d0d49fcf3b5f4f91d13fdbfddea3f9d5dfafd7
dabarre <dabarre@inf.upv.es>
2020-09-16 17:06:07 +0200

    Fix function

--- webgl/js/paintPoints.js
+++ webgl/js/paintPoints.js
@@ -39,7 +39,7 @@ function main() {
         }
 
         // Set clear color
-        gl.clearColor(1.0, 0.0, 0.0, 1.0);
+        gl.clearColor(0.0, 0.2, 0.4, 1.0);
         // Clear canvas
         gl.clear(gl.COLOR_BUFFER_BIT);
         
